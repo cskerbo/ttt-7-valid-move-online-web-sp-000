910998797bfb1d98095a523f5ce8288dd7f38ed1
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-09 20:48:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if position_taken?(board, index) == false
     board[index].between?("0", "8")
     true
-  elsif board[index].between("0", "8") == false
+  elsif position_taken?(board, index) == true
     false
   end
 end
